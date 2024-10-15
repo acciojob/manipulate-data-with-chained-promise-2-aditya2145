59bd3d8a83cd754fe7a7c9d07e2b2c6cc68ac846
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-10-15 17:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+function manipulateData() {
+    const promise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    });
+
+    promise
+		.then((numbers) => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    const evenNumbers = numbers.filter((num) => num % 2 === 0);
+                    document.getElementById('output').innerText = evenNumbers.join(', ');
+                    resolve(evenNumbers);
+                }, 1000);
+            });
+        })
+        .then((evenNumbers) => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    const doubledNumbers = evenNumbers.map((num) => num * 2);
+                    document.getElementById('output').innerText = doubledNumbers.join(', ');
+                    resolve(doubledNumbers);
+                }, 2000);
+            });
+        })
+        .catch((err) => console.error('Error:', err));  
+}
+
+manipulateData();
