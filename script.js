e6d0949307717959ee7fa3014379b4cc17c04623
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-10-15 17:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function manipulateData() {
             return new Promise((resolve) => {
                 setTimeout(() => {
                     const evenNumbers = numbers.filter((num) => num % 2 === 0);
-                    document.getElementById('output').innerText = evenNumbers.join(', ');
+                    document.getElementById('output').innerText = evenNumbers.join(',');
                     resolve(evenNumbers);
                 }, 1000);
             });
@@ -20,7 +20,7 @@ function manipulateData() {
             return new Promise((resolve) => {
                 setTimeout(() => {
                     const doubledNumbers = evenNumbers.map((num) => num * 2);
-                    document.getElementById('output').innerText = doubledNumbers.join(', ');
+                    document.getElementById('output').innerText = doubledNumbers.join(',');
                     resolve(doubledNumbers);
                 }, 2000);
             });
